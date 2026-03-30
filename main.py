598c8ef896cba424ba8599e42a50aa95b3c32a3c
Hunter Pallister <hijrcm848@gmail.com>
2026-03-30 13:01:37 -0700

    Added Hunters part of the assignment.

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
+#variables
+#H
+try:
+    length = int(input("Enter the length of the the object as a integer: "))
+    width = int(input("Enter the width of the the object as a integer: "))
+    height = int(input("Enter the height of the the object as a integer: "))
+except:
+    ValueError
+    print("Invalid Input")
+
 #Function 1
 # Returns Area of Rectangle
+#H
+def rect_area(length, width):
+    a = length*width
+    return a
+
 
 #Function 2
 # Returns Surface Area of Rectangular Solid
 
+
 # Request the dimension of a solid rectangular object
-
-length = int(input("Enter the length of the the object as a integer: "))
-width = int(input("Enter the width of the the object as a integer: "))
-height = int(input("Enter the height of the the object as a integer: "))
-
-print("Length = ", length, "Width = ", width, "Height = ", height)
-print("Total Surface Area = ", str(rect_surface_area(length, width, height)))
-print("Area of the rectangle: " + str(rect_area(length, width)))
+#H
+try:
+    print (f"Length = {length} Width = {width} Height = {height}")
+    print("Total Surface Area = ", str(rect_surface_area(length, width, height)))
+    print("Area of the rectangle: " + str(rect_area(length, width)))
+except:
+    NameError
+    
