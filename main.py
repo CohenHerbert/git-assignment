f21b8bf31e6cdd2b60c4ac76c1eb9da36ac87b1f
Thunderdudeski <thunderdudeski@icloud.com>
2026-04-02 22:24:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,15 @@
 # 1. rect_area (length, width) which will return the area of a rectangle
 # 2. rect_solid_area (length, width, height) which will return the area of a solid rectangular object
 
-# The following four lines are just there to make the code work without errors until functions are added
-def rect_solid_area(x, y, z):
-   return 1
-length = 1; width = 1; height = 1
-rect_solid_area (length, width, height)
+def rect_area(x, y):
+   area = x*y
+   return area
 
 # Request the dimension of a solid rectangular object
 length = int(input("Enter the length of the the object as in integer: "))
 width = int(input("Enter the width of the the object as in integer: "))
-height = int(input("Enter the height of the the object as in integer: "))
 
-surface_area = rect_solid_area(length, width, height)
-
-print("Length = ", length, "Width = ", width, "Height = ", height)
-print("Total Surface Area = ", surface_area)
-
+print(rect_area(length, width))
 
 #end
 
