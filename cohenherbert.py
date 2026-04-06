b3bf1d0a1085b1eb5ab20bbc91466881433d97c3
Cohen Herbert <66497306+CohenHerbert@users.noreply.github.com>
2026-04-06 01:16:06 -0700

    Create functions and fix main file logic

---
+++ cohenherbert.py
@@ -0,0 +1,5 @@
+def rect_area(length, width):
+    return length * width
+
+def rect_surface_area(length, width, height):
+    return 2 * (rect_area(length, width) + rect_area(length, height) + rect_area(width, height))
